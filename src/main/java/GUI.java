4e98eaa678a886ff69b898c294e7c5bd553f9869
Robert Watling <rwatling0902@gmail.com>
2018-10-15 17:48:00 -0400

    Initial Commit for GUI
    I had a gui, but then it got deleted somehow

---
+++ src/main/java/GUI.java
@@ -0,0 +1,14 @@
+import javafx.application.Application;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.image.Image;;
+
+public class GUI extends Application {
+	 public static void main (String[] args) {
+		 launch(args);
+	 }
+	 
+	 @Override
+	 public void start(Stage primaryStage) {
+		 
+	 }
+ }
