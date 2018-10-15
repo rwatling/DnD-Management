465e26e01f10578e9180eda03130a10ef5bf416a
Jacob Abeyta <jacobabeyta1999@gmail.com>
2018-10-15 15:08:16 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 1b0c762a3d5c1cf8f9de2ad5a80a78e97eff1c08.

---
+++ src/main/java/Work.java
@@ -0,0 +1,12 @@
+package main.java;
+
+public class Work {
+
+	public Work() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int returnOne( ) {
+		return 1;
+	}
+}
---
+++ src/test/java/WorkTest.java
@@ -0,0 +1,17 @@
+//package test.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import main.java.Work;
+
+public class WorkTest {
+
+	@Test
+	public void testReturnOne() {
+		Work w = new Work();
+		assertEquals(1, w.returnOne() );
+	}
+
+}
