f5426ebe41650d8f59880c978113ed660f9812e3
Jacob Abeyta <jacobabeyta1999@gmail.com>
2018-10-24 14:49:35 -0400

    Added getters & setters for basic identifiers to PC class

--- src/main/java/PC.java
+++ src/main/java/PC.java
@@ -11,4 +11,28 @@ public class PC {
 	String name;
 	String align;
 	String gender;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String s) {
+		name = s;
+	}
+	
+	public String getAlignment() {
+		return align;
+	}
+	
+	public void setAlignment(String s) {
+		align = s;
+	}
+	
+	public String getGender() {
+		return gender;
+	}
+	
+	public void setGender(String s) {
+		gender = s;
+	}
 }
