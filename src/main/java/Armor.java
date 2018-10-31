df4b0f29ef5be26aeecf1da895f73c87279870f1
Andrew Menzl <ammenzl7@gmail.com>
2018-10-31 14:41:55 -0400

    Added code for armor, needs testing

---
+++ src/main/java/Armor.java
@@ -0,0 +1,71 @@
+package main.java;
+
+public class Armor {
+	String name;
+	int type; // 0 for no armor, 1 for light armor, 2 for medium armor, 3 for heavy armor.
+	int armorClass;
+	int dexterity;
+	boolean disadvantageOnStealthChecks;
+	
+	public Armor(String name, int type, int currentArmorClass, int dexterity, boolean disadvantageOnStealthChecks) {
+		this.name = name;
+		this.type = type;
+		armorClass = calculateArmorClass(type, currentArmorClass);
+		this.dexterity = dexterity;
+		this.disadvantageOnStealthChecks = disadvantageOnStealthChecks;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getType() {
+		return type;
+	}
+	
+	public void setType(int type) {
+		this.type = type;
+	}
+	
+	public int getArmorClass() {
+		return armorClass;
+	}
+	
+	public int getDexterity() {
+		return dexterity;
+	}
+	
+	public void setArmorClassAndDexterity(int currentArmorClass, int dexterity) {
+		this.dexterity = dexterity;
+		armorClass = calculateArmorClass(type, currentArmorClass);
+	}
+	
+	public boolean getDisadvantageOnStealthChecks() {
+		return disadvantageOnStealthChecks;
+	}
+	
+	public void setDisadvantageOnStealthChecks(boolean disadvantageOnStealthChecks) {
+		this.disadvantageOnStealthChecks = disadvantageOnStealthChecks;
+	}
+	
+	public int calculateArmorClass(int type, int currentArmorClass) {
+		if (type == 1) { // Light Armor
+			return currentArmorClass + dexterity;
+		} else if (type == 2) { // Medium Armor
+			if (dexterity > 2) {
+				return currentArmorClass + 2;
+			} else {
+				return currentArmorClass + dexterity;
+			}
+		} else if (type == 3) { // Heavy Armor
+			return currentArmorClass;
+		}
+		
+		// No Armor
+		return 0;
+	}
+}
