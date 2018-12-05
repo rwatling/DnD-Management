75774ab231ff05bb7b2e28a918bc652a925555ff
Andrew Menzl <ammenzl7@gmail.com>
2018-12-05 14:31:11 -0500

    Added Urchin, tests run successfully

--- src/main/java/PC.java
+++ src/main/java/PC.java
@@ -47,7 +47,7 @@ public class PC {
 	
 	String[] allRaces = new String [] {"Hill Dwarf", "Mountain Dwarf", "High Elf", "Wood Elf", "Dark Elf", "Lightfoot Halfling", "Stout Halfling", "Human", "Dragonborn", "Gnome", "Half-Elf", "Half-Orc", "Tiefling"};
 	String[] allClasses = new String[] {"Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"};
-	String[] allBackgrounds = new String[] {"Acolyte", "Charlatan", "Criminal", "Entertainer", "Folk Hero", "Guild Artisan", "Hermit", "Noble", "Sage", "Sailor", "Soldier"};
+	String[] allBackgrounds = new String[] {"Acolyte", "Charlatan", "Criminal", "Entertainer", "Folk Hero", "Guild Artisan", "Hermit", "Noble", "Sage", "Sailor", "Soldier", "Urchin"};
 	ArrayList<String> languages = new ArrayList<String>();
 	ArrayList<String> weaponArmorProfs = new ArrayList<String>();
 	
@@ -271,6 +271,9 @@ public class PC {
 			break;
 		case "Soldier":
 			pcBackground = new Soldier();
+			break;
+		case "Urchin":
+			pcBackground = new Urchin();
 			break;
 		}
 	}
--- src/test/java/ProjectTest.java
+++ src/test/java/ProjectTest.java
@@ -1355,4 +1355,43 @@ public class ProjectTest {
 
 		assertEquals("Military Rank", testBackground.getFeature());
 	}
+	
+	//Urchin Tests--------------------------------------------------
+	@Test
+	public void testUrchin() {
+		PC test = new PC();
+		test.setBackground("Urchin");
+		
+		assertEquals("Urchin", test.getBackgroundTitle());
+		
+		assertEquals(0, test.getHP());
+		
+		Background testBackground = new Urchin();
+		
+		ArrayList<String> skillProfs = new ArrayList<String>();
+		skillProfs.add("Sleight of Hand");
+		skillProfs.add("Stealth");
+		
+		assertEquals(skillProfs, testBackground.getSkillProfs());
+		
+		assertEquals(0, testBackground.getNumLangs());
+		
+		ArrayList<String> toolProfs = new ArrayList<String>();
+		toolProfs.add("Disguise Kit");
+		toolProfs.add("Thieves' Tools");
+		
+		assertEquals(toolProfs, testBackground.getToolProfs());
+		
+		ArrayList<String> equipment = new ArrayList<String>();
+		equipment.add("Small Knife");
+		equipment.add("Map of the City You Grew Up In");
+		equipment.add("Pet Mouse");
+		equipment.add("Token to Remember Your Parents By");
+		equipment.add("Set of Common Clothes");
+		equipment.add("Belt Pouch Containing 10 gp");
+		
+		assertEquals(equipment, testBackground.getEquip());
+
+		assertEquals("City Secrets", testBackground.getFeature());
+	}
 }
